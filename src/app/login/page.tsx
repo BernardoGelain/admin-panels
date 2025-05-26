7fd08ab74e269ed0059e90b37f1fcf7d7336e1ea
Bernardo Gelain Dariva <bernardogelaindariva@MacBook-Pro-de-Bernardo.local>
2025-05-26 16:06:44 -0300

    feat: favicon e metadata

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import { LoginPage } from "~/page-components/login-page/login-page";
 
 export const metadata: Metadata = {
-  title: "Zukese - Login",
+  title: "Marcante - Login",
 };
 
 export default LoginPage;
